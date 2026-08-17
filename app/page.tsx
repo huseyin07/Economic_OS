aa2820eb3e02372ccba7d2365d25e1633f29077e
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-17 12:45:40 +0300

    Enable Economic OS system experience

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import { About, FAQ, Hero } from "../components/sections";
 import { EconomicPulse } from "../components/economic-pulse";
 import { Footer, Header } from "../components/shell";
 import { SystemProgress } from "../components/system-progress";
+import { SystemExperience } from "../components/system-experience";
 
 export default function Home() {
-  return <><Header /><SystemProgress /><main><Hero /><About /><EconomicPulse /><FAQ /></main><Footer /></>;
+  return <><Header /><SystemProgress /><SystemExperience /><main><Hero /><About /><EconomicPulse /><FAQ /></main><Footer /></>;
 }
