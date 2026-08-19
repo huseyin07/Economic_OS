34c0c98674475f77f5d11b59a02a4fcf2a0f6b49
Hüseyin <74729922+huseyin07@users.noreply.github.com>
2026-08-19 10:05:05 +0300

    Load professional Build control styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { EconomicPulse } from "../components/economic-pulse";
 import { Footer, Header } from "../components/shell";
 import { SystemProgress } from "../components/system-progress";
 import { SystemExperience } from "../components/system-experience";
+import "./build-controls.css";
 
 export default function Home() {
   return <><Header /><SystemProgress /><SystemExperience /><main><Hero /><About /><div id="build"><EconomicPulse /></div><FAQ /></main><Footer /></>;
